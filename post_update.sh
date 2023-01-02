f29c56d081bf8fea90d1b16cb99e01000194ffd0
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-03 02:26:48 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- post_update.sh
+++ post_update.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# $1 Path to the new version.
-# $2 Path to the old version.
-
-cp -a -n $2/build.gradle $1/
+version https://git-lfs.github.com/spec/v1
+oid sha256:f7e34b9e4ef736d742f40a89aa4a3e18586fe89e3010639c78236c1dea54242b
+size 103
